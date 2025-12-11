ed3aa3cee857cb68fffdcd081018db0cc9982216
sushanthacharya2003 <acharyasushanth37@gmail.com>
2025-12-11 23:36:04 +0530

    add full application controller + model with resume support

--- src/controllers/application.controller.js
+++ src/controllers/application.controller.js
@@ -1,10 +1,61 @@
 // Application controller placeholders
+import { applyForJob,getApplicationsByJobId,getApplicationsByUserEmail } from "../models/application.model";
+import { getJobById } from "../models/job.model.js";
 
-exports.apply = (req, res) => {
-  res.send('Apply for job - implement logic');
+export const showApplyForm = (req, res) => {
+  const jobId = Number(req.params.jobId);
+  const job=getJobById(jobId);
+  if (!job)
+    {
+      return res.status(404).send('Job not found')
+  }
+  else{
+  res.render('applications/apply-job', { job });
+  }
 };
 
-exports.listApplications = (req, res) => {
-  res.send('List applications - implement logic');
-};
+export const submitApplication = (req, res) => {
+  const jobId = Number( req.params.jobId);
+  const job=getJobById(jobId);
+  if (!job)
+    {
+      return res.status(404).send('Job not found')
+  }
+  const resumeFile= req.file? req.file.fileName: null;
+  const {name, email,coverLetter}= req.body;
+  const newApplication={
+    id: Date.now(),
+    jobId,
+    applicantName:name,
+    userEmail:email,
+    resumeFile,
+    coverLetter,
+    appliedAt: new Date()
+  };
+  applyForJob(newApplication);
+  return res.render("msgPage",{message:"Application submitted successfully!"});
+}
+
+export const listApplicationsForJob = (req, res) => {
+
+  const jobId = Number(req.params.jobId);
+  const job=getJobById(jobId);
+  if (!job) {
+    return res.status(404).send('No applications found for this job');
+  }
+  const applications = getApplicationsByJobId(jobId);
+  res.render('applications/applicants', { job, applications });
+}
+
+export const listApplicationsForUser = (req, res) => {
+
+  const userEmail = req.session.user.email;
+  const applications = getApplicationsByUserEmail(userEmail);
+  if (applications.length === 0) {
+    return res.status(404).send('No applications found for this user');
+  }
+  else{
+  res.render('applications/applicant-list', { applications, userEmail });
+}
+}
 
--- src/models/application.model.js
+++ src/models/application.model.js
@@ -20,3 +20,4 @@ export const getApplicationsByUserEmail = (email) => {
     (application) => application.userEmail === email
   );
 };
+
