05ba940a8648d3cc59d37a202e79401e52b1991c
sushanthacharya2003 <acharyasushanth37@gmail.com>
2025-12-14 16:21:32 +0530

    file duplication problem in the vs code so facing issues while editing code

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ import app from "./app.js";
 const PORT = 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT} and link http://localhost:${PORT}/ `);
 });
